ea39021ecc49345e00987d50c3f2a9d4d0ecf60e
Moritz Gansel <moritzgansel.mg@gmail.com>
2020-03-26 14:14:58 +0100

    Background tracking test : ok

--- app/src/main/java/de/trackcovidcluster/Controller.java
+++
@@ -1,4 +0,0 @@
-package de.trackcovidcluster;
-
-public class Controller {
-}
--- app/src/main/java/de/trackcovidcluster/status/StatusActivity.kt
+++ app/src/main/java/de/trackcovidcluster/status/StatusActivity.kt
@@ -1,13 +1,19 @@
 package de.trackcovidcluster.status
 
 import android.Manifest
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
+import android.content.ServiceConnection
 import android.content.pm.PackageManager
 import android.location.LocationManager
 import android.os.Bundle
+import android.os.IBinder
+import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.annotation.NonNull
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -19,6 +25,8 @@ import de.trackcovidcluster.status.Constants.INFECTED
 import de.trackcovidcluster.status.Constants.MAYBE_INFECTED
 import de.trackcovidcluster.status.Constants.NOT_INFECTED
 import de.trackcovidcluster.status.Constants.STATUS_KEY
+import de.trackcovidcluster.tracking.BackgroundLocationService
+import de.trackcovidcluster.tracking.BackgroundLocationService.LocationServiceBinder
 import kotlinx.android.synthetic.main.activity_status.*
 import javax.inject.Inject
 
@@ -32,6 +40,9 @@ class StatusActivity : AppCompatActivity() {
     // region members
     private lateinit var mViewModel: ChangeStatusViewModel
     private val PERMISSIONS_REQUEST = 100
+    var gpsService: BackgroundLocationService? = null
+    var mTracking = false
+
     @Inject
     lateinit var mViewModelFactory: ViewModelProvider.Factory
     private lateinit var mCurrentStatusImage: ImageView
@@ -76,6 +87,51 @@ class StatusActivity : AppCompatActivity() {
             updateStatus(status = status)
         }
 
+        /**
+         * Testing
+         * TODO Start tracking
+         */
+        //prepare service
+        val intent = Intent(this.getApplication(), BackgroundLocationService::class.java)
+        this.getApplication().startService(intent)
+        this.getApplication()
+            .bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE)
+
+        fun stopTracking() {
+            mTracking = false;
+            gpsService?.stopTracking();
+        }
+
+        fun startTracking() {
+            //check for permission
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                gpsService?.startTracking();
+                mTracking = true;
+            } else {
+                ActivityCompat.requestPermissions(this, arrayOf(ACCESS_FINE_LOCATION), 200);
+            }
+        }
+
+        fun onRequestPermissionsResult(
+            requestCode: Int,
+            @NonNull permissions: Array<String?>?,
+            @NonNull grantResults: IntArray
+        ) {
+            if (permissions != null) {
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+            }
+            if (requestCode == 200) {
+                if (grantResults.size > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                ) {
+                    startTracking()
+                }
+            }
+        }
+
+        /**
+         * TODO: This should change on Server poll :
+         */
         maybeInfectedContainer.setOnClickListener {
             startActivity(
                 Intent(this, ChangeStatusActivity::class.java)
@@ -114,6 +170,26 @@ class StatusActivity : AppCompatActivity() {
                         INFECTED
                     )
             )
+        }
+    }
+
+    val serviceConnection: ServiceConnection = object : ServiceConnection {
+
+        override fun onServiceConnected(
+            className: ComponentName,
+            service: IBinder
+        ) {
+            val name = className.className
+            if (name.endsWith("BackgroundLocationService")) {
+                gpsService = (service as LocationServiceBinder).getService()
+                Log.d("GPS Ready", "!!");
+            }
+        }
+
+        override fun onServiceDisconnected(className: ComponentName) {
+            if (className.className == "BackgroundLocationService") {
+                gpsService = null
+            }
         }
     }
 
