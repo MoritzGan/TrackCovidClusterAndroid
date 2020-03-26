a8b05cde400cbf40f04585da71c9a03811768f32
Moritz Gansel <moritzgansel.mg@gmail.com>
2020-03-26 10:42:08 +0100

    Added Location Listener

---
+++ app/src/main/java/de/trackcovidcluster/tracking/BackgroundLocationService.java
@@ -0,0 +1,135 @@
+package de.trackcovidcluster.tracking;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Binder;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
+
+public class BackgroundLocationService extends Service {
+    private final LocationServiceBinder binder = new LocationServiceBinder();
+    private static final String TAG = "BackgroundLocationServi";
+    private LocationListener mLocationListener;
+    private LocationManager mLocationManager;
+    private NotificationManager notificationManager;
+    private final int LOCATION_INTERVAL = 500;
+    private final int LOCATION_DISTANCE = 10;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+    private class LocationListener implements android.location.LocationListener {
+        private Location lastLocation = null;
+        private final String TAG = "LocationListener";
+        private Location mLastLocation;
+
+        public LocationListener(String provider) {
+            mLastLocation = new Location(provider);
+        }
+
+        @Override
+        public void onLocationChanged(Location location) {
+            mLastLocation = location;
+            Log.i(TAG, "LocationChanged: " + location);
+            // Save to local DB
+            Toast.makeText(BackgroundLocationService.this, "LAT: " + location.getLatitude() + "\n LONG: " + location.getLongitude(), Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            Log.e(TAG, "onProviderDisabled: " + provider);
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            Log.e(TAG, "onProviderEnabled: " + provider);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            Log.e(TAG, "onStatusChanged: " + status);
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+        return START_NOT_STICKY;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public void onCreate() {
+        Log.i(TAG, "onCreate");
+        startForeground(12345678, getNotification());
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mLocationManager != null) {
+            try {
+                mLocationManager.removeUpdates(mLocationListener);
+            } catch (Exception ex) {
+                Log.i(TAG, "fail to remove location listeners, ignore", ex);
+            }
+        }
+    }
+
+    private void initializeLocationManager() {
+        if (mLocationManager == null) {
+            mLocationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+        }
+    }
+
+    public void startTracking() {
+        initializeLocationManager();
+        mLocationListener = new LocationListener(LocationManager.GPS_PROVIDER);
+
+        try {
+            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_INTERVAL, LOCATION_DISTANCE, mLocationListener);
+
+        } catch (java.lang.SecurityException ex) {
+            // Log.i(TAG, "fail to request location update, ignore", ex);
+        } catch (IllegalArgumentException ex) {
+            // Log.d(TAG, "gps provider does not exist " + ex.getMessage());
+        }
+
+    }
+
+    public void stopTracking() {
+        this.onDestroy();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private Notification getNotification() {
+
+        NotificationChannel channel = new NotificationChannel("channel_01", "My Channel", NotificationManager.IMPORTANCE_DEFAULT);
+
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
+
+        Notification.Builder builder = new Notification.Builder(getApplicationContext(), "channel_01").setAutoCancel(true);
+        return builder.build();
+    }
+
+
+    public class LocationServiceBinder extends Binder {
+        public BackgroundLocationService getService() {
+            return BackgroundLocationService.this;
+        }
+    }
+}
