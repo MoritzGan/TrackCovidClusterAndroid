ff3f00c84632d43ac1c109f230b9c753f0cdc70c
Moritz Gansel <moritzgansel.mg@gmail.com>
2020-04-01 17:18:44 +0200

    working on ble tracking

--- app/src/main/java/de/trackcovidcluster/changeStatus/ReturnCookiesCallback.java
+++ app/src/main/java/de/trackcovidcluster/changeStatus/ReturnCookiesCallback.java
@@ -5,5 +5,5 @@ import java.util.List;
 import de.trackcovidcluster.models.Cookie;
 
 public interface ReturnCookiesCallback {
-    public void returnCookiesCallback(List<Cookie> sensorData);
+    public void returnCookiesCallback(List<String> sensorData);
 }
